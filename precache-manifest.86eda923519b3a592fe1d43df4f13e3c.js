311e0d15d6960e064dcfd5c88269ccab2b5c72dc
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-09-29 18:25:56 +0530

    Updates

---
+++ precache-manifest.86eda923519b3a592fe1d43df4f13e3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850251b900c8e0ed86a9d855ccdf88d0",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "da8148eaf8b717826c6c",
+    "url": "/react-todo-list/static/js/2.77900d69.chunk.js"
+  },
+  {
+    "revision": "417966665448c72ea7dd",
+    "url": "/react-todo-list/static/js/main.5f9298a2.chunk.js"
+  },
+  {
+    "revision": "b9eb7a41dabfef57a8fc",
+    "url": "/react-todo-list/static/js/runtime-main.96b6467a.js"
+  }
+]);
